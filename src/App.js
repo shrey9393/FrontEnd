7941dfef226f5696eabe6602342d34d62e6f9a44
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-11-15 19:25:15 +0530

    page 4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Page1 from "./components/Page1";
 import Page2 from "./components/Page2";
 import Page3 from "./components/Page3";
+import Page4 from "./components/Page4";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Page1 />
       <Page2 />
       <Page3 />
+      <Page4 />
     </>
   );
 }
