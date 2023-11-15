affc5fce20fac48829f0d238beee1ce0952f25a1
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-11-15 22:09:01 +0530

    page 5 and 6

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Page1 from "./components/Page1";
 import Page2 from "./components/Page2";
 import Page3 from "./components/Page3";
 import Page4 from "./components/Page4";
+import Page5 from "./components/Page5";
+import Page6 from "./components/Page6";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Page2 />
       <Page3 />
       <Page4 />
+      <Page5 />
+      <Page6 />
     </>
   );
 }
---
+++ src/components/Page5.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+function Page5() {
+  return (
+    <div class="container text-center bg-purple-100 mt-10">
+      <div class="row row-cols-2 row-cols-lg-2 g-2 g-lg-3">
+        <div class="col">
+          <div class="p-3">Work with us</div>
+        </div>
+        <div class="col">
+          <div class="p-3">ahead</div>
+        </div>
+        <div class="col">
+          <div class="p-3">about</div>
+          <div class="p-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptas
+            in cumque dolore dolorem deleniti eum quam, laudantium enim labore
+            suscipit laborum, blanditiis culpa praesentium recusandae aliquam
+            quidem esse? Iste!
+          </div>
+          <div class="p-3">Product</div>
+          <div class="p-3">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis
+            quidem nesciunt ab dolorum animi quasi sunt consequatur voluptate
+            nam distinctio, magni eveniet aliquam iure vitae ipsum! Adipisci
+            quidem aliquid vero.
+          </div>
+        </div>
+        <div class="col">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+          doloribus ut soluta laudantium animi amet fugit a laboriosam maiores
+          debitis pariatur minus dicta assumenda sapiente eum, obcaecati esse
+          voluptatum. Rem odit error tenetur explicabo. Ullam doloribus aliquam
+          officia reprehenderit iure minima aperiam facere molestiae officiis.
+          Sunt architecto in unde voluptates nemo eos eveniet magnam placeat
+          maxime, nobis illum impedit dignissimos ipsum veritatis magni cum?
+          Recusandae reiciendis rerum, sequi nesciunt dolore id doloremque
+          optio, nulla error, nam illo autem sit. Aut nulla sunt sequi,
+          blanditiis adipisci maiores nihil unde molestiae et reiciendis
+          perferendis reprehenderit fugiat architecto nobis consequuntur
+          laudantium? Saepe, aut!
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page5;
